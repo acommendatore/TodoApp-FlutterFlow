7243256154f55ae6d5c22acab71f9c7faee8e41e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-11 03:00:55 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/backend/firebase/firebase_config.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+
+Future initFirebase() async {
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyBIYTYQP6BtojfjH81lt1wgXZRpTIti4Sw",
+            authDomain: "to-do-app-xqwrgr.firebaseapp.com",
+            projectId: "to-do-app-xqwrgr",
+            storageBucket: "to-do-app-xqwrgr.firebasestorage.app",
+            messagingSenderId: "250918738228",
+            appId: "1:250918738228:web:d6a3a242c9d7d51e2aed61"));
+  } else {
+    await Firebase.initializeApp();
+  }
+}
---
+++ lib/index.dart
@@ -0,0 +1,4 @@
+// Export pages
+export '/pages/login/login_widget.dart' show LoginWidget;
+export '/pages/onboarding/onboarding_widget.dart' show OnboardingWidget;
+export '/pages/tasks/tasks_widget.dart' show TasksWidget;
